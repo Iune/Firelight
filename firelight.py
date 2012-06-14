18e2de7720f4c9bfca81c436ded2f0cbf1f4a3c0
Aditya Duri <ma-on@live.com>
2012-06-14 17:24:40 -0400

    First Commit; Adding Base Files for Configuration Tool

---
+++ firelight.py
@@ -0,0 +1,41 @@
+a = open('Files/c1.txt')
+c1 = [line.strip() for line in open('Files/c1.txt')]
+a.close()
+
+b = open('Files/c2.txt')
+c2 = [line.strip() for line in open('Files/c2.txt')]
+b.close()
+
+c = open('Files/c3.txt')
+c3 = [line.strip() for line in open('Files/c3.txt')]
+c.close()
+
+d = open('Files/c4.txt')
+c4 = [line.strip() for line in open('Files/c4.txt')]
+d.close()
+
+e = open('Files/c5.txt')
+c5 = [line.strip() for line in open('Files/c5.txt')]
+e.close()
+
+f = open('Files/c6.txt')
+c6 = [line.strip() for line in open('Files/c6.txt')]
+f.close()
+
+g = open('Files/c7.txt')
+c7 = [line.strip() for line in open('Files/c7.txt')]
+g.close()
+
+h = open('Files/c8.txt')
+c8 = [line.strip() for line in open('Files/c8.txt')]
+h.close()
+
+i = open('Files/c9.txt')
+c9 = [line.strip() for line in open('Files/c9.txt')]
+i.close()
+
+j = open('Files/c10.txt')
+c10 = [line.strip() for line in open('Files/c10.txt')]
+j.close()
+
+
