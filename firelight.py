b5716900cd0fd241ebe376c04e0a20a4393a1c21
Aditya Duri <ma-on@live.com>
2012-06-15 16:05:24 -0400

    Program now calculates and stores the points given to each song

--- firelight.py
+++ firelight.py
@@ -38,9 +38,45 @@ j = open('Files/c10.txt')
 c10 = [line.strip() for line in open('Files/c10.txt')]
 j.close()
 
-print "Hi, add a number" 
-num1 = int(raw_input())
-print "Hi, add another number"
-num2 = int(raw_input())
-num3 = num1 + num2
-print num1 + num2
+
+
+print "Please enter the points given to Song 1: " + c1[0]
+s1p = int(raw_input())
+c1[1] = int(c1[1]) + s1p
+
+print "Please enter the points given to Song 2: " + c2[0]
+s2p = int(raw_input())
+c2[1] = int(c2[1]) + s2p
+
+print "Please enter the points given to Song 3: " + c3[0]
+s3p = int(raw_input())
+c3[1] = int(c3[1]) + s3p
+
+print "Please enter the points given to Song 4: " + c4[0]
+s4p = int(raw_input())
+c4[1] = int(c4[1]) + s4p
+
+print "Please enter the points given to Song 5: " + c5[0]
+s5p = int(raw_input())
+c5[1] = int(c5[1]) + s5p
+
+print "Please enter the points given to Song 6: " + c6[0]
+s6p = int(raw_input())
+c6[1] = int(c6[1]) + s6p
+
+print "Please enter the points given to Song 7: " + c7[0]
+s7p = int(raw_input())
+c7[1] = int(c7[1]) + s7p
+
+print "Please enter the points given to Song 8: " + c8[0]
+s8p = int(raw_input())
+c8[1] = int(c8[1]) + s8p
+
+print "Please enter the points given to Song 9: " + c9[0]
+s9p = int(raw_input())
+c9[1] = int(c9[1]) + s9p
+
+print "Please enter the points given to Song 10: " + c10[0]
+s10p = int(raw_input())
+c10[1] = int(c10[1]) + s10p
+
